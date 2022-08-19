7873a07dbfdd2b373cc9e7af03510ea94bdb81d9
jhongarciamedina <jhonfarru.game@gmail.com>
2022-08-18 19:41:42 -0500

    SUBIENDO PROYECTO

---
+++ src/test/java/com/idat/EFEurekaServer/EfEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idat.EFEurekaServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EfEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
